f823fbf38032b9c269da43fdb06037f9d3fac695
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-09-28 11:36:47 +0300

    fix project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,16 +1,18 @@
 import styled  from 'styled-components'
-import React, { Component, FC, useState } from 'react'
-
-import { Grid } from '@material-ui/core';
+import React, {  FC, useState } from 'react'
+import { Divider, Grid } from '@material-ui/core';
+import ReactImg from '../images/skills/react.png'
 enum ProjectType{FrontEnd="FrontEnd",Backend="Backend"};
 interface Iprops{
 image:string
 title:string
 desc:string 
 type:ProjectType
+tools:string[]
+icons:string[]
 
 }
-const ProjectCard:FC<Iprops> = ({image,title,desc,type}) => {
+const ProjectCard:FC<Iprops> = ({image,title,desc,type,tools,icons}) => {
     const StyledCardPicture=styled.div`
     background-image: linear-gradient(to right bottom,
                   #064730,
@@ -25,42 +27,75 @@ const ProjectCard:FC<Iprops> = ({image,title,desc,type}) => {
         <ProjectCardcontainer>
             <StyledCardPicture></StyledCardPicture>
             <StledProjectCardTitle>
-          <span >
+          <span>
           {title}
           </span>
         </StledProjectCardTitle>
         <ProjectDetails>
           <h5> {type} </h5>
-          <p>{desc}</p>
-      
-      
-          </ProjectDetails>
+          <p>{desc}</p> 
+         </ProjectDetails>
+         
+          <ToolsContainer>
+          <Grid container spacing={2}>
+            {tools.map((tool,index)=>{
+              return  <Grid item xs={3}>
+              <img src={icons[index]} alt="logo" style={{width:'25px',height:'20px'}}/>
+              <p>{tool}</p>
+              </Grid>
+            })}
+          </Grid>      
+          </ToolsContainer>
+          <Divider/>
+          <StyledButton>Details</StyledButton>
+        
+
         </ProjectCardcontainer>
     )
 }
 
 const ProjectCardcontainer=styled.div`
 width: 100%;
- height: 35rem;
-background-color: red;
+ height: 32rem;
+background-color: #fff;
 position: relative;
 overflow: hidden;
 box-shadow: 0 1.5rem 4rem rgba($color: #000000, $alpha: 0.2);
- border-radius:4px;
+border-radius:4px;
+ 
 `
-
+const StyledButton=styled.button`
+border:none;
+outline:none;
+padding:.5rem;
+background-color: #23d997;
+color:white;
+text-align: center;
+margin: 1rem;
+&:hover{
+background-color: white;
+border: 1px solid #23d997;
+color:#23d997
+}
+`
+ const ToolsContainer=styled.div`
+text-align: center;
+ margin:1.5rem;
+ p{
+   font-size: .8rem;
+ }
+ `
 const StledProjectCardTitle=styled.h4`
-    font-size: 2rem;
+    font-size: 1.5rem;
     font-weight: 200  ;
     text-transform: uppercase;
-    text-align: right;
-    
+    text-align: right; 
     position: absolute;
     top: 10rem;
     right: 2rem;
-    width: 60%;
+    width: 70%;
    span{
-      padding: 1rem 1rem;
+      padding: .3rem 1rem;
       box-decoration-break: clone;
       color: white;
         background-image: linear-gradient(
@@ -68,13 +103,20 @@ const StledProjectCardTitle=styled.h4`
           #064730,
             #23d997
         );
-    
     }
- 
 `;
 const ProjectDetails=styled.div`
 display:flex;
 flex-direction: column;
 justify-content: center;
+margin:1rem;
+h5{
+  font-size: 1.5rem;
+  color:#FE8800;
+}
+p{
+  margin-top: .5rem;
+  color: #d1caca;
+}
 `
 export default ProjectCard
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,37 @@ import  styled  from 'styled-components'
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import { Grid } from '@material-ui/core'
-import project1 from '../images/laptop.jpg'
+import project1 from '../images/laptop.jpg';
+import reactImg from "../images/skills/react.png";
+import htmlimg from "../images/skills/html.png";
+import cssimg from "../images/skills/css.png";
+import sassImg from "../images/skills/sass.png";
+import bootstrapImg from "../images/skills/bootstrap-4.svg";
+import dockerImg from "../images/skills/docker.png";
+import firebaseImg from "../images/skills/firebase.png";
+import formikImg from "../images/skills/formik.png";
+import gitImg from "../images/skills/git.png";
+import javascriptImg from "../images/skills/javascript.png";
+import jiraImg from "../images/skills/jira.png";
+import mongoImg from "../images/skills/mongo.png";
+import nodeImg from "../images/skills/node.png";
+import oauthImg from "../images/skills/oauth.png";
+import reduxsagaImg from "../images/skills/redux-saga.png";
+import reduxImg from "../images/skills/redux.png";
+import sqlImg from "../images/skills/sql.png";
+import stripeImg from "../images/skills/stripe.png";
+import styledComponentsImg from "../images/skills/styledComponents.png";
+import typescriptImg from "../images/skills/typescript.png";
+import antImg from "../images/skills/ant.png";
+import materialuiImg from "../images/skills/material-ui.svg";
+import tailwindImg from "../images/skills/tailwind.png";
 const Projects = () => {
     enum ProjectType{FrontEnd="FrontEnd",Backend="Backend"};
     return (
         <StyledProjectsContainer>
              <Grid container spacing={5}>
                  <Grid item xs={3}>
-                 <ProjectCard image={project1} title="project 1" desc="this is description" type={ProjectType.FrontEnd}/>
+                 <ProjectCard image={project1} title="Natural Tours Booking" desc="The Front End part of Booking Tours online project" type={ProjectType.FrontEnd} tools={["React","Redux","Redux-saga","SCSS"]} icons={[reactImg,reduxImg,reduxsagaImg,sassImg]}/>
                  </Grid>
               
              </Grid>
