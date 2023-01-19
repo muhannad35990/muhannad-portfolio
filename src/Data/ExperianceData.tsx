edee7bb108c987ec8da246cda7ed10d1d27736b2
Muhannad Hammada <muhannad35990@gmail.com>
2023-01-19 23:37:57 +0300

    change cv

--- src/Data/ExperianceData.tsx
+++ src/Data/ExperianceData.tsx
@@ -1,24 +1,44 @@
 export const ExperianceData = [
   {
-    date: "Feb 2022 - present",
+    date: "Sep 2022 - present",
+    title: "Senior Frontend Web Developer",
+    company: "IOTIVATA/ Reinstil GmbH & Co. KG",
+    site: "https://www.iotivata.com/",
+    roles: [
+      "Convert designs and wireframes into responsive reusable components and pages",
+      "Using NextJs ,ReactJs and making asynchronous API calls in order to maintain consistency across client, server, and native environments maintainable and testable code in addition to fixing bugs and adding new features to existing applications."
+    ]
+  },
+  {
+    date: "Aug 2022 - present",
+    title: "Senior Frontend Web Developer (Part time)",
+    company: "Acquire.Fi",
+    site: "acquire.fi",
+    roles: [
+      "Acquire is the first online marketplace dedicated solely to cryptocurrency businesses, where you can buy, sell, and browse through hundreds of available businesses. Start your own bitcoin business and cash in on the rising demand for digital currency. Acquire can help you whether you want to buy an existing company or start a new one from scratch.",
+      "Using NextJs to build the frontend and link to the smart contracts related to the investment process and to the backend with the dashboard, marketplace and pools."
+    ]
+  },
+  {
+    date: "Feb 2022 - Apr 2022",
     title: "Front-End web Developer (Part time)",
     company: "Pickuppb",
     site: "https://pickuppb.com/",
     roles: [
       "Pickuupb is a social network for a new game in USA where players can communicate and arrange a match and record the results.",
-      "Converting designs and wireframes into responsive reusable components and pages Using ReactJs",
-    ],
+      "Converting designs and wireframes into responsive reusable components and pages Using ReactJs"
+    ]
   },
   {
-    date: "Nov 2021 - present",
+    date: "Nov 2021 - Oct 2022",
     title: "Front-End web Developer",
     company: "Cloud systems SARL",
     site: "www.cloudsystems.tech",
     roles: [
       "Convert designs and wireframes into responsive reusable components and pages",
       "Using NextJs as framework and making asynchronous API calls in order to maintain consistency across client, server, and native environments",
-      "Making maintainable and testable code in addition to fixing bugs and adding new features to existing applications",
-    ],
+      "Making maintainable and testable code in addition to fixing bugs and adding new features to existing applications"
+    ]
   },
   {
     date: "Sept 2020 - Oct 2021",
@@ -28,8 +48,8 @@ export const ExperianceData = [
     roles: [
       "Convert designs and wireframes into responsive reusable components therefore producing an efficient and easily testable app with less overall cost",
       "Used libraries such as Redux to manage the web application’s state, and make asynchronous API calls in order to maintain consistency across client, server, and native environments",
-      " Increased customer satisfaction by collaborating with different teams in an Agile environment to write clean, maintainable, and testable code in addition to fixing bugs and adding new features to existing applications",
-    ],
+      " Increased customer satisfaction by collaborating with different teams in an Agile environment to write clean, maintainable, and testable code in addition to fixing bugs and adding new features to existing applications"
+    ]
   },
   {
     date: "Mar 2019 - present",
@@ -38,7 +58,7 @@ export const ExperianceData = [
     site: "",
     roles: [
       "Developing applications following a 3-tier architecture (Frontend, Backend, Database) using Javascript and JSON",
-      " Making use of React.js for Frontend, Node.js as a server-side framework, and MongoDB as the document database",
-    ],
-  },
-];
+      " Making use of React.js for Frontend, Node.js as a server-side framework, and MongoDB as the document database"
+    ]
+  }
+]
